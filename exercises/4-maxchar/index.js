d6df5103258c9c5e86da61e796bcb519dd691579
rayGreycloud <rayGreycloud@gmail.com>
2017-11-16 14:10:20 -0800

    Add solution to exercise 4 - max characters

--- exercises/4-maxchar/index.js
+++ exercises/4-maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+  
+  for (let char of str) {
+    // if (!chars[char]) {
+    //   chars[char] = 1;
+    // } else {
+    //   chars[char]++;
+    // }
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;    
+    }
+  }
+  
+  return maxChar;
+}
 
 module.exports = maxChar;
