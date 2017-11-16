836890d2292a4ddb26694a2030370651d31937d7
rayGreycloud <rayGreycloud@gmail.com>
2017-11-16 12:53:50 -0800

    Add grider's alternate but not as good solution

--- exercises/2-palindrome/index.js
+++ exercises/2-palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// initial solution
+// function palindrome(str) {
+//   return str === str.split('').reverse().join('');  
+// }
+
+// Refactored solution
+const palindrome = str => str === str.split('').reverse().join('');
+
+// grider's alternate but not best solution
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - 1 - i];
+//   });
+// }
 
 module.exports = palindrome;
