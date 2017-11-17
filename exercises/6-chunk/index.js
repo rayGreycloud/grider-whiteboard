835dd1eec93e5a59e7e5f74df4524b12291b5361
rayGreycloud <rayGreycloud@gmail.com>
2017-11-17 10:33:17 -0800

    My solution for exercise 6 - array chunking

--- exercises/6-chunk/index.js
+++ exercises/6-chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
-
+function chunk(array, size) {
+  let chunked = [];
+  
+  while (array.length > 0) {
+    if (array.length < size) {
+      size = array.length;
+    }
+    let chunk = array.splice(0, size);
+    chunked.push(chunk); 
+  }
+  
+  return chunked;
+}
+  
 module.exports = chunk;
