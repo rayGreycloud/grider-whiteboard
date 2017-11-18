e73d8cd3258900e3a9b6c65725d253f8dbdd057a
rayGreycloud <rayGreycloud@gmail.com>
2017-11-18 07:29:07 -0800

    Instructor's alternate solution to exercise 8

--- exercises/8-capitalize/index.js
+++ exercises/8-capitalize/index.js
@@ -7,20 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-// My solution
 function capitalize(str) {
-  const words = [];  
-  const array = str.split(' ');
-  
-  for (let index in array) {
-    let capitalized = array[index].slice(0, 1)
-      .toUpperCase()
-      .concat(array[index].slice(1));
-    
-    words.push(capitalized);
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
-  
+
   return words.join(' ');
 }
+
+// My solution
+// function capitalize(str) {
+//   const words = [];  
+//   const array = str.split(' ');
+// 
+//   for (let index in array) {
+//     let capitalized = array[index].slice(0, 1)
+//       .toUpperCase()
+//       .concat(array[index].slice(1));
+// 
+//     words.push(capitalized);
+//   }
+// 
+//   return words.join(' ');
+// }
 
 module.exports = capitalize;
