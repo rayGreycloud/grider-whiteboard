0a3aae195f87f997fd7a3839e0d514cbcf0ba230
rayGreycloud <rayGreycloud@gmail.com>
2017-11-19 12:07:36 -0800

    Add iterative solution to exercise 10 pyramid

--- exercises/10-pyramid/index.js
+++ exercises/10-pyramid/index.js
@@ -14,6 +14,34 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // calculate number of columns for N levels
+  const totalColumns = 2 * n - 1;
+  // calculate index of midpoint of level 
+  const midpoint = Math.floor(totalColumns / 2);
+      
+  // iterate thru rows
+  for (let row = 0; row < n; row++) {
+    // initialize level string
+    let level = '';
+    // determine start/end indexes for pyramid on this level 
+    let leftmost = midpoint - row;
+    let rightmost = midpoint + row;
+    
+    // iterate thru columns of level
+    for (let column = 0; column < totalColumns ; column++) {
+      // determine if column is within pyramid indexes
+      if (leftmost <= column && rightmost >= column) {
+        // add '#' to level string
+        level += '#';
+      } else {
+        // add ' ' to level string 
+        level += ' ';
+      }
+    }
+    // level complete - print it
+    console.log(level);
+  }  
+}
 
 module.exports = pyramid;
