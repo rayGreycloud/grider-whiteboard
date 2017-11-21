5cf19a51a32a6379b699bad249df33fbe245c8cf
rayGreycloud <rayGreycloud@gmail.com>
2017-11-21 09:20:59 -0800

    Add LinkedList constructor and insertFirst methods and test

--- exercises/18-linkedlist/index.js
+++ exercises/18-linkedlist/index.js
@@ -3,12 +3,21 @@
 // See 'directions' document
 
 class Node {
-  constructor(data, next = null) {
+  constructor(data, next) {
     this.data = data;
-    this.next = next;
+    this.next = next || null;
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  
+  insertFirst(data) {
+    this.head = new Node(data, this.head);    
+  }
+  
+}
 
 module.exports = { Node, LinkedList };
