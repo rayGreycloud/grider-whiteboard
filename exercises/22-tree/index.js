77e9e7858a7d18bff9899b31a9fb4247493f5304
rayGreycloud <rayGreycloud@gmail.com>
2017-11-24 16:38:01 -0800

    Complete exercise 22 tree with implementation of tree class

--- exercises/22-tree/index.js
+++ exercises/22-tree/index.js
@@ -27,6 +27,34 @@ class Node {
   }
 }
 
-class Tree {}
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  
+  traverseBF(fn) {
+    let arr = [this.root];
+    
+    while(arr.length) {
+      const node = arr.shift();
+      
+      arr.push(...node.children);
+      
+      fn(node);  
+    }
+  }
+  
+  traverseDF(fn) {
+    let arr = [this.root];
+    
+    while(arr.length) {
+      const node = arr.shift();
+      
+      arr.unshift(...node.children);
+      
+      fn(node);      
+    }
+  }  
+}
 
 module.exports = { Tree, Node };
