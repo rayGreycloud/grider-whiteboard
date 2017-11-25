91eb8330bab8940e893021a25f6c8a30223a7227
rayGreycloud <rayGreycloud@gmail.com>
2017-11-25 10:50:08 -0800

    Implement Node constructor and insert method

--- exercises/24-bst/test.js
+++ exercises/24-bst/test.js
@@ -9,33 +9,35 @@ test('Node can insert correctly', () => {
   node.insert(5);
   node.insert(15);
   node.insert(17);
+  node.insert(10);
 
   expect(node.left.data).toEqual(5);
   expect(node.right.data).toEqual(15);
   expect(node.right.right.data).toEqual(17);
+  expect(node.left.right.data).toEqual(10);
 });
 
-test('Contains returns node with the same data', () => {
-  const node = new Node(10);
-  node.insert(5);
-  node.insert(15);
-  node.insert(20);
-  node.insert(0);
-  node.insert(-5);
-  node.insert(3);
-
-  const three = node.left.left.right;
-  expect(node.contains(3)).toEqual(three);
-});
-
-test('Contains returns null if value not found', () => {
-  const node = new Node(10);
-  node.insert(5);
-  node.insert(15);
-  node.insert(20);
-  node.insert(0);
-  node.insert(-5);
-  node.insert(3);
-
-  expect(node.contains(9999)).toEqual(null);
-});
+// test('Contains returns node with the same data', () => {
+//   const node = new Node(10);
+//   node.insert(5);
+//   node.insert(15);
+//   node.insert(20);
+//   node.insert(0);
+//   node.insert(-5);
+//   node.insert(3);
+// 
+//   const three = node.left.left.right;
+//   expect(node.contains(3)).toEqual(three);
+// });
+// 
+// test('Contains returns null if value not found', () => {
+//   const node = new Node(10);
+//   node.insert(5);
+//   node.insert(15);
+//   node.insert(20);
+//   node.insert(0);
+//   node.insert(-5);
+//   node.insert(3);
+// 
+//   expect(node.contains(9999)).toEqual(null);
+// });
